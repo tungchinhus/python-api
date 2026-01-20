8b3a1d757f7e25e2f83e0381b74431b5d67ef373
chinhdo12 <tungchinhus@gmail.com>
2026-01-20 14:04:49 +0700

    update

--- venv/Lib/site-packages/numpy/_typing/_nested_sequence.py
+++ venv/Lib/site-packages/numpy/_typing/_nested_sequence.py
@@ -4,12 +4,14 @@ from __future__ import annotations
 
 from typing import (
     Any,
-    Iterator,
-    overload,
     TypeVar,
     Protocol,
     runtime_checkable,
+    TYPE_CHECKING,
 )
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
 
 __all__ = ["_NestedSequence"]
 
@@ -62,12 +64,7 @@ class _NestedSequence(Protocol[_T_co]):
         """Implement ``len(self)``."""
         raise NotImplementedError
 
-    @overload
-    def __getitem__(self, index: int, /) -> _T_co | _NestedSequence[_T_co]: ...
-    @overload
-    def __getitem__(self, index: slice, /) -> _NestedSequence[_T_co]: ...
-
-    def __getitem__(self, index, /):
+    def __getitem__(self, index: int, /) -> _T_co | _NestedSequence[_T_co]:
         """Implement ``self[x]``."""
         raise NotImplementedError
 
