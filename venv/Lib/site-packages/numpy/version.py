81777c36f19dc65e2c6e2948ca764bcc32365934
chinhdo12 <tungchinhus@gmail.com>
2026-01-22 13:45:31 +0700

    update

--- venv/Lib/site-packages/numpy/version.py
+++ venv/Lib/site-packages/numpy/version.py
@@ -2,10 +2,10 @@
 """
 Module to expose more detailed version info for the installed `numpy`
 """
-version = "2.2.6"
+version = "2.4.1"
 __version__ = version
 full_version = version
 
-git_revision = "2b686f659642080e2fc708719385de6e8be0955f"
+git_revision = "d24bb7f48d3b0e3471c68f1309c130d0b65ee72a"
 release = 'dev' not in version and '+' not in version
 short_version = version.split("+")[0]
